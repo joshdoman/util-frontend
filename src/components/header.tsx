87bb5e19956390fb5234312b5cd51e31aa7300c7
Josh Doman <joshsdoman@gmail.com>
2023-12-19 20:52:19 -0500

    clean up

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import NextLink from "next/link"
 import { Flex, useColorModeValue, Heading, LinkBox, LinkOverlay } from '@chakra-ui/react'
 
 const siteTitle="Util Testnet"
-export default function Header() {
+export default function Header() {  
 
   return (
     <Flex as='header' bg={useColorModeValue('gray.100', 'gray.900')} p={4} alignItems='center'>
@@ -16,7 +16,4 @@ export default function Header() {
       </LinkBox>
     </Flex>
   )
-
-  // <Spacer />
-  // <Button >Button for Account </Button>
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,29 +2,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 import { Layout } from 'components/layout'
-import { WagmiConfig, createConfig, configureChains, sepolia } from 'wagmi'
-import { publicProvider } from 'wagmi/providers/public'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const { publicClient, webSocketPublicClient } = configureChains(
-    [sepolia],
-    [publicProvider()],
-  )
-
-  const config = createConfig({
-    autoConnect: true,
-    publicClient,
-    webSocketPublicClient,
-  })
-
   return (
-    <WagmiConfig config={config}>
-      <ChakraProvider>
-        <Layout>
-        <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
-    </WagmiConfig>
+    <ChakraProvider>
+      <Layout>
+      <Component {...pageProps} />
+      </Layout>
+    </ChakraProvider>
   )
 }
 
